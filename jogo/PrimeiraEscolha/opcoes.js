954a1a08c669afff2af5f825b5d1fcccd1904e8f
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-04-11 01:25:56 -0300

    Criando mesmo efeito para segunda opção

--- jogo/PrimeiraEscolha/opcoes.js
+++ jogo/PrimeiraEscolha/opcoes.js
@@ -31,3 +31,20 @@ opcao1.addEventListener("click", function(){
 opcao2.addEventListener("mouseover", function(){
     papel.play();
 });
+
+opcao2.addEventListener("click", function(){
+    
+    if(cont == 0){
+        opcao2.textContent = `prosseguir`;
+        texto.textContent = `Devia ser alguém te confundindo, ao prosseguir você avista um ladrão roubando o dinheiro de um turista e consegue pará-lo com facilidade, o prende em um poste até a segurança chegar. Que bom que deu tempo.`
+        cont = 1;
+    }else if(cont == 1){
+        location.href = "/home.html";
+    };
+    
+    opcao1.classList.add("desaparecer");
+    setTimeout(
+        () => {
+            opcao1.remove();
+        },1000);
+});
