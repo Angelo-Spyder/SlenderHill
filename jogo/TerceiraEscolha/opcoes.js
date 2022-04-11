292a4097473625c648cdf40ac1ce9ab501bbc06c
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-04-11 02:18:33 -0300

    Criando funcionalidades de escolha na terceira decisão

--- jogo/TerceiraEscolha/opcoes.js
+++ jogo/TerceiraEscolha/opcoes.js
@@ -4,8 +4,12 @@ var opcao2 = document.querySelector("#opcao2");
 
 var texto = document.querySelectorAll(".texto-historia")
 var papel = document.querySelector(".paper-slide");
+var janela = document.querySelector(".janela")
+var estatica = document.querySelector("static")
 
 var cont = 0;
+
+//janela.play()
 
 opcao1.addEventListener("mouseover", function(){
     papel.play();
@@ -15,8 +19,9 @@ opcao1.addEventListener("click", function(){
     
     if(cont == 0){
         opcao1.textContent = `prosseguir`;
-        texto[0].textContent = `Drakard coça a cabeça e diz.`
-        texto[1].textContent = `"Bom não há muito o que sei, além daquilo, mas eu posso afirmar que as crianças estão mais estranhas que os adultos, inventando histórias e dizendo pra não confiarem em adultos, desconfio que seja mais uma brincadeira delas ou um surto coletivo."`
+        texto[0].textContent = `Pela janela há alguém correndo pra longe e você escuta um barulho de chiado que logo fica atrás de você, e quando se vira não vê direito além de um borrão, e por já estar na janela você pula dela e cai no chão...`
+        texto[1].textContent = `Acorda no dia seguinte, sua arma sumiu e a janela continua quebrada.`
+        texto[2].innerHTML = "";
         cont = 1;
     }else if(cont == 1){
         location.href = "../TerceiraEscolha/Terceira.html";
@@ -34,14 +39,17 @@ opcao2.addEventListener("mouseover", function(){
 });
 
 opcao2.addEventListener("click", function(){
-    
+    estatica.play()
     if(cont == 0){
-        opcao2.textContent = `prosseguir`;
-        texto[0].textContent = `Você segue até o quarto e não há ninguém lá no caminho, mas há um bilhete no chão na entrada do seu quarto que diz.`;
-        texto[1].textContent = `"Nada é real, tudo isso é farsa, saia logo da casa, saia de tudo, e nunca mais volte."`
+        opcao2.textContent = `Tentar novamente`;
+        texto[0].textContent = `Ao pegar a arma um chiado do outro lado da porta surge e você dispara quando algo borrado surge a sua frente com tom ameaçador, sua arma é levada e você leva um empurrão e uma mordida no pescoço, conseguindo ver pouco, apenas um terno e depois nada...`;
+
+        texto[1].textContent = `GAME OVER"`;
+
+        texto[2].innerHTML = ""
         cont = 1;
     }else if(cont == 1){
-        location.href = "../TerceiraEscolha/Terceira.html";
+        location.href = "/home.html";
     };
     
     opcao1.classList.add("desaparecer");
