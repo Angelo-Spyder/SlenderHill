ee689939d8fffed4846d67be620e6d3164bce795
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-04-11 01:23:34 -0300

    Criando função de escolha e adicionando efeito de apagar escolha

--- jogo/PrimeiraEscolha/opcoes.js
+++ jogo/PrimeiraEscolha/opcoes.js
@@ -2,10 +2,30 @@
 var opcao1 = document.querySelector("#opcao1");
 var opcao2 = document.querySelector("#opcao2");
 
+var texto = document.querySelector(".texto-historia")
 var papel = document.querySelector(".paper-slide");
+
+var cont = 0;
 
 opcao1.addEventListener("mouseover", function(){
     papel.play();
+});
+
+opcao1.addEventListener("click", function(){
+    
+    if(cont == 0){
+        opcao1.textContent = `prosseguir`;
+        texto.textContent = `Ao falar com a pessoa ela te entrega um bilhete "Não confie nos adultos" e some antes que possa perguntar. Enquanto isso alguém assalta um turista e consegue escapar.`
+        cont = 1;
+    }else if(cont == 1){
+        location.href = "/home.html";
+    };
+    
+    opcao2.classList.add("desaparecer");
+    setTimeout(
+        () => {
+            opcao2.remove();
+        },1000);
 });
 
 opcao2.addEventListener("mouseover", function(){
