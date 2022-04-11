657e3bc5c38898b72f42dee130c2f8b3d3615f61
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-04-11 00:30:54 -0300

    configurando nome do arquivo js, adicionando efeito sonoro ao html

---
+++ jogo/PrimeiraEscolha/opcoes.js
@@ -0,0 +1,13 @@
+
+var opcao1 = document.querySelector("#opcao1");
+var opcao2 = document.querySelector("#opcao2");
+
+var papel = document.querySelector(".paper-slide");
+
+opcao1.addEventListener("mouseover", function(){
+    papel.play();
+});
+
+opcao2.addEventListener("mouseover", function(){
+    papel.play();
+});
