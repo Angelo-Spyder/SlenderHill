6c0b51831cab321ece8a84159370ff5ec1cc6486
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-04-11 02:25:30 -0300

    Javascript da função final

---
+++ jogo/Final/opcoes.js
@@ -0,0 +1,13 @@
+
+var opcao1 = document.querySelector("#opcao1");
+
+var papel = document.querySelector(".paper-slide");
+
+opcao1.addEventListener("mouseover", function(){
+    papel.play();
+});
+
+opcao1.addEventListener("click", function(){
+    location.href = "/home.html"
+});
+
